b2483f2e476d6c8b761aaaf4839acf22df2859d7
yue zeng <euphar1992@gmail.com>
2022-03-04 20:21:36 -0500

    'part2'

---
+++ app/src/main/java/com/example/yflixster/DetailActivity.kt
@@ -0,0 +1,90 @@
+package com.example.yflixster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.RatingBar
+import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
+
+private const val TAG = "DetailActivity"
+private const val YOUTUBE_API_KEY = "AIzaSyCO6mUmZTIpM_ygFq6XJvoHbKaKaFWz8mc"
+private const val TRAILER_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+class DetailActivity : YouTubeBaseActivity() {
+
+    private lateinit var tvTitle: TextView
+    private lateinit var tvOverview: TextView
+    private lateinit var ratingBar: RatingBar
+    private lateinit var ytPlayerView: YouTubePlayerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+        tvTitle = findViewById(R.id.tvTitle)
+        tvOverview = findViewById(R.id.tvOverview)
+        ratingBar = findViewById(R.id.rbVoteAverage)
+        ytPlayerView = findViewById(R.id.player)
+
+
+        val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
+
+        Log.i(TAG,"Movie is $movie")
+        tvTitle.text = movie.title
+        tvOverview.text = movie.overview
+
+        ratingBar.rating = movie.voteAverage.toFloat()
+
+        val client = AsyncHttpClient()
+        client.get(TRAILER_URL.format(movie.movieId), object: JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.e(TAG,"onFailure $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                val results = json.jsonObject.getJSONArray("results")
+                if (results.length() == 0){
+                    Log.w(TAG,"no movie found")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+
+                initializeYoutube(youtubeKey)
+            }
+
+        })
+    }
+
+    private fun initializeYoutube(youtubeKey: String) {
+        ytPlayerView.initialize(YOUTUBE_API_KEY, object: YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                player?.cueVideo(youtubeKey);
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+
+    }
+}
--- app/src/main/java/com/example/yflixster/MovieAdapter.kt
+++ app/src/main/java/com/example/yflixster/MovieAdapter.kt
@@ -1,14 +1,19 @@
 package com.example.yflixster
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivities
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
+const val MOVIE_EXTRA ="MOVIE_EXTRA"
+private const val TAG ="MovieAdapter"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
 
@@ -26,16 +31,36 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
 
             Glide.with(context).load(movie.posterImageUrl).into(ivPoster)
+
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[adapterPosition]
+
+            Toast.makeText(context, movie.title, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(context, DetailActivity::class.java)
+
+            intent.putExtra(MOVIE_EXTRA,movie)
+
+            context.startActivities(arrayOf(intent))
+
 
         }
     }
